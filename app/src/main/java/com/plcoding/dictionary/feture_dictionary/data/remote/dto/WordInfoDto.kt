3e4cfb0d805d657c203d374fd7839d8b80af0080
Eddy <moura@ciandt.com>
2022-12-07 09:31:39 -0300

    Configura app module

--- app/src/main/java/com/plcoding/dictionary/feture_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/plcoding/dictionary/feture_dictionary/data/remote/dto/WordInfoDto.kt
@@ -4,16 +4,10 @@ import com.google.gson.annotations.SerializedName
 import com.plcoding.dictionary.feture_dictionary.data.local.entity.WordInfoEntity
 
 data class WordInfoDto(
-    @SerializedName("license")
-    val license: License,
     @SerializedName("meaningDtos")
     val meanings: List<MeaningDto>,
     @SerializedName("phonetic")
     val phonetic: String,
-    @SerializedName("phoneticDtos")
-    val phonetics: List<PhoneticDto>,
-    @SerializedName("sourceUrls")
-    val sourceUrls: List<String>,
     @SerializedName("word")
     val word: String
 ) {
